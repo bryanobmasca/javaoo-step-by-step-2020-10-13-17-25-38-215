4b5b6db3de763401dd58d2a791560fbdaea3ba83
BRYAN BERNARD OBMASCA (SAPE-MIS-ISD-OOCLL/MNL) <OBMASBR@OOCL.COM>
2020-10-14 21:31:22 +0800

    [OO Step by Step][Bryan] feature: Modify function assignLeader() and create function appenMember() in Klass Class

--- src/main/java/practice10/Klass.java
+++ src/main/java/practice10/Klass.java
@@ -1,8 +1,12 @@
 package practice10;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klass {
     private int klass;
     private Student leader;
+    private List<Student> studentList = new ArrayList<>();
 
     public Klass(int klass) {
         this.klass = klass;
@@ -17,7 +21,7 @@ public class Klass {
     }
 
     public Student assignLeader(Student leader) {
-        if (!leader.getClass().equals(getNumber())){
+        if (!studentList.contains(leader)){
             System.out.print("It is not one of us.\n");
             return null;
         }
@@ -30,4 +34,7 @@ public class Klass {
         return leader;
     }
 
+    public void appendMember(Student member) {
+        studentList.add(member);
+    }
 }
