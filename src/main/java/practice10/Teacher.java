8437b258ea53cb836617018e72163384d895721b
BRYAN BERNARD OBMASCA (SAPE-MIS-ISD-OOCLL/MNL) <OBMASBR@OOCL.COM>
2020-10-14 22:04:25 +0800

    [OO Step by Step][Bryan] feature: Create function introduce() in Teacher Class

--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,6 +1,9 @@
 package practice10;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.List;
 
 public class Teacher extends Person{
     private LinkedList<Klass> klassLinkedList;
@@ -13,5 +16,17 @@ public class Teacher extends Person{
 
     public LinkedList<Klass> getClasses() {
         return klassLinkedList;
+    }
+
+    public String introduce(){
+        String klassNumber = "Class ";
+        for (Klass classesList : klassLinkedList){
+            klassNumber += classesList.getNumber() + ", ";
+        }
+        if(klassNumber.endsWith(", "))
+        {
+            klassNumber = klassNumber.substring(0,klassNumber.length() - 2);
+        }
+        return String.format("%s I am a Teacher. I teach %s.", super.introduce(), klassNumber);
     }
 }
