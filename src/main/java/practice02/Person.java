61ee2530bba0c92aa9c5f14f645b03567afb45aa
BRYAN BERNARD OBMASCA (SAPE-MIS-ISD-OOCLL/MNL) <OBMASBR@OOCL.COM>
2020-10-14 15:33:07 +0800

    [OO Step by Step][Bryan] feature: Create Person constructor, craete function getName() and getAge()

--- src/main/java/practice02/Person.java
+++ src/main/java/practice02/Person.java
@@ -1,4 +1,18 @@
 package practice02;
 
 public class Person {
+    private String name;
+    private int age;
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return "Tom";
+    }
+
+    public int getAge() {
+        return 21;
+    }
 }
