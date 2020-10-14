a5e87b8a1f3230d5cbc849ffe0bd0d148e3db1fb
BRYAN BERNARD OBMASCA (SAPE-MIS-ISD-OOCLL/MNL) <OBMASBR@OOCL.COM>
2020-10-14 21:40:43 +0800

    [OO Step by Step][Bryan] feature: Inherit Person class to Teacher class, add Teacher constructor, add function getClasses() in Teacher Class

--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,4 +1,17 @@
 package practice10;
 
-public class Teacher {
+import java.util.LinkedList;
+
+public class Teacher extends Person{
+    private LinkedList<Klass> klassLinkedList;
+
+    public Teacher(int id, String name, int age, LinkedList<Klass> klassLinkedList) {
+        super(id, name, age);
+        this.klassLinkedList = klassLinkedList;
+
+    }
+
+    public LinkedList<Klass> getClasses() {
+        return klassLinkedList;
+    }
 }
