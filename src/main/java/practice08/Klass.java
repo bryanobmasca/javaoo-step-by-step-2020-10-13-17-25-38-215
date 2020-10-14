3b6877deb5ee38bda3165470c4248f2038cd851d
BRYAN BERNARD OBMASCA (SAPE-MIS-ISD-OOCLL/MNL) <OBMASBR@OOCL.COM>
2020-10-14 18:30:27 +0800

    [OO Step by Step][Bryan] feature: Create instance of Student as leader, create function assignLeader and getLeader()

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -2,6 +2,7 @@ package practice08;
 
 public class Klass {
     private int klass;
+    private Student leader;
 
     public Klass(int klass) {
         this.klass = klass;
@@ -13,5 +14,13 @@ public class Klass {
 
     public String getDisplayName() {
         return String.format("Class %d", getNumber());
+    }
+
+    public Student assignLeader(Student jerry) {
+        return leader = jerry;
+    }
+
+    public Student getLeader() {
+        return leader;
     }
 }
